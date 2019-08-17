30a60760febf15d009b6e25d90c79878df66e853
Chet Budhathoki <okchet@gmail.com>
2019-08-17 10:03:44 -0600

    fixed reference and index file

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,10 @@
-// Initialize Firebase
   // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyDvddeAa3GtZT-IaEPFAwBKgtBiEwPpHHk",
     authDomain: "trainschedular-4a30f.firebaseapp.com",
     databaseURL: "https://trainschedular-4a30f.firebaseio.com",
     projectId: "trainschedular-4a30f",
-    storageBucket: "",
+    storageBucket: "trainschedular-4a30f.appspot.com",
     messagingSenderId: "623307320782",
     appId: "1:623307320782:web:05577d464caecd76"
   };
@@ -18,29 +17,31 @@
   // Create an on click function that adds trax to the top table
   $("#add-trax-btn").on("click", function(event) {
     event.preventDefault();
+
+    console.log(event);
   
     // create variables with the user input from form
-    var trnName = $("#trax-name-input").val().trim();
-    var trnDest = $("#destination-input").val().trim();
-    var firstTrnTime = $("#first-trax-input").val().trim();
-    var trnFreq = $("#frequency-input").val().trim();
+    var trxName = $("#trax-name-input").val().trim();
+    var trxDest = $("#destination-input").val().trim();
+    var firstTrxTime = $("#first-trax-input").val().trim();
+    var trxFreq = $("#frequency-input").val().trim();
   
     // create a temporary object for holding the new trax data
-    var newTrn = {
-      name: trnName,
-      destination: trnDest,
-      firstTime: firstTrnTime,
-      frequency: trnFreq
+    var newTrx = {
+      name: trxName,
+      destination: trxDest,
+      firstTime: firstTrxTime,
+      frequency: trxFreq
     };
   
     // upload the new trax data to the database
-    database.ref().push(newTrn);
+    database.ref().push(newTrx);
   
     // console log the values that were just pushed to the database
-    console.log(newTrn.name);
-    console.log(newTrn.destination);
-    console.log(newTrn.firstTime);
-    console.log(newTrn.frequency);
+    console.log(newTrx.name);
+    console.log(newTrx.destination);
+    console.log(newTrx.firstTime);
+    console.log(newTrx.frequency);
   
     // clear the form values after values have been stored
     $("#trax-name-input").val("");
@@ -54,32 +55,32 @@
     console.log(childSnapshot.val());
   
     // store snapshot changes in variables
-    var trnName = childSnapshot.val().name;
-    var trnDest = childSnapshot.val().destination;
-    var firstTrnTime = childSnapshot.val().firstTime;
-    var trnFreq = childSnapshot.val().frequency;
+    var trxName = childSnapshot.val().name;
+    var trxDest = childSnapshot.val().destination;
+    var firstTrxTime = childSnapshot.val().firstTime;
+    var trxFreq = childSnapshot.val().frequency;
   
     // log the values
-    console.log(trnName);
-    console.log(trnDest);
-    console.log(firstTrnTime);
-    console.log(trnFreq);
+    console.log(trxName);
+    console.log(trxDest);
+    console.log(firstTrxTime);
+    console.log(trxFreq);
   
     // process for calculating the Next Arrival and Minutes Away fields...
     // make sure the first trax time is after the eventual current time
-    var firstTrnTimeConv = moment(firstTrnTime, "hh:mm a").subtract(1, "years");
+    var firstTrxTimeConv = moment(firstTrxTime, "hh:mm a").subtract(1, "years");
     // store variable for current time
     var currentTime = moment().format("HH:mm a");
     console.log("Current Time:" + currentTime);
     // store variable for difference of current time and first trax time
-    var trnTimeCurrentTimeDiff = moment().diff(moment(firstTrnTimeConv), "minutes");
+    var trxTimeCurrentTimeDiff = moment().diff(moment(firstTrxTimeConv), "minutes");
     // store the time left
-    var timeLeft = trnTimeCurrentTimeDiff % trnFreq;
+    var timeLeft = trxTimeCurrentTimeDiff % trxFreq;
     // calculate and store the minutes until next trax arrives
-    var minutesAway = trnFreq - timeLeft;
+    var minutesAway = trxFreq - timeLeft;
     // calculate the next arriving trax
     var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
   
     // add the data into the DOM/html
-    $("#trax-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDest + "</td><td>" + trnFreq + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+    $("#trax-table > tbody").append("<tr><td>" + trxName + "</td><td>" + trxDest + "</td><td>" + trxFreq + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
   });
